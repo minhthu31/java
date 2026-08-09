486e44efad7598b281edda8606b2a9b7180c13fc
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-10 02:55:16 +0700

    Create ErrorResponse.java

---
+++ src / main / java / vn / edu / cnpm / projectsupport / | common/exception/ErrorResponse.java
@@ -0,0 +1,27 @@
+package vn.edu.cnpm.projectsupport.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private Map<String, String> errors;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String message, Map<String, String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+        this.timestamp = LocalDateTime.now();
+    }
+}
