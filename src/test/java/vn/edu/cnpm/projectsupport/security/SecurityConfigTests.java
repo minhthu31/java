fcd9326d0de63e6378ff1ef9795178cd55c8f91c
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-10 15:32:49 +0700

    CNPM-42 Spring Security Configuration
    Removed specific properties from SpringBootTest annotation in SecurityConfigTests.

--- src/test/java/vn/edu/cnpm/projectsupport/security/SecurityConfigTests.java
+++ src/test/java/vn/edu/cnpm/projectsupport/security/SecurityConfigTests.java
@@ -13,16 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@SpringBootTest(properties = {
-    "app.jwt.secret=401b63e22363964121a328323a2d20741facd722d56214d1f60087413063f915",
-    "app.jwt.expiration-ms=86400000",
-    "spring.flyway.enabled=false",
-    "spring.jpa.hibernate.ddl-auto=create-drop",
-    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password="
-})
+@SpringBootTest
 class SecurityConfigTests {
 
     @Autowired
