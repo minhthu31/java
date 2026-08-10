ed23c69b506b19cbac7e85c33e7cd2f2e49172d7
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-10 13:56:57 +0700

    CNPM-41 Building an Authentication Service
    Added @EntityGraph annotation to optimize queries for user role.

--- src/main/java/vn/edu/cnpm/projectsupport/identity/repository/UserRepository.java
+++ src/main/java/vn/edu/cnpm/projectsupport/identity/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package vn.edu.cnpm.projectsupport.identity.repository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.edu.cnpm.projectsupport.identity.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"role"})
     Optional<User> findByUsernameIgnoreCase(String username);
+
+    @EntityGraph(attributePaths = {"role"})
     Optional<User> findByEmailIgnoreCase(String email);
     boolean existsByUsernameIgnoreCase(String username);
     boolean existsByEmailIgnoreCase(String email);
