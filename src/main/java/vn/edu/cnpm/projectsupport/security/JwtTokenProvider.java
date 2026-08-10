d0a7c4880c9084a9aeba2b8ceaa63783d3a743cb
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-10 15:32:09 +0700

    CNPM-42 Spring Security Configuration

--- src/main/java/vn/edu/cnpm/projectsupport/security/JwtTokenProvider.java
+++ src/main/java/vn/edu/cnpm/projectsupport/security/JwtTokenProvider.java
@@ -15,7 +15,7 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    @Value("${app.jwt.secret}")
+    @Value("${app.jwt.secret:401b63e22363964121a328323a2d20741facd722d56214d1f60087413063f915}")
     private String jwtSecret;
 
     @Value("${app.jwt.expiration-ms:86400000}")
