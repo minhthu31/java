dd8e6bf6aaf9556a0629249a47b19fb00eec227a
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-10 20:20:22 +0700

    CNPM-42 Spring Security Configuration
    Removed specific properties from SpringBootTest annotation.

--- src/test/java/vn/edu/cnpm/projectsupport/ProjectSupportBackendApplicationTests.java
+++ src/test/java/vn/edu/cnpm/projectsupport/ProjectSupportBackendApplicationTests.java
@@ -5,12 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest(properties = {
-    "spring.jpa.hibernate.ddl-auto=create-drop",
-    "spring.flyway.enabled=false",
-    "app.jwt.secret=401b63e22363964121a328323a2d20741facd722d56214d1f60087413063f915",
-    "app.jwt.expiration-ms=86400000"
-})
+@SpringBootTest
 class ProjectSupportBackendApplicationTests {
 
     @Test
