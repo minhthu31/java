698cbff2c0b33db3696e7055e4baa2b505f6ebfe
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-12 20:56:51 +0700

    CNPM-42 Spring Security Configuration

--- src/main/java/vn/edu/cnpm/projectsupport/config/JacksonConfig.java
+++ src/main/java/vn/edu/cnpm/projectsupport/config/JacksonConfig.java
@@ -1,18 +1,21 @@
 package vn.edu.cnpm.projectsupport.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-        return builder -> builder
-                .modules(new JavaTimeModule())
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
