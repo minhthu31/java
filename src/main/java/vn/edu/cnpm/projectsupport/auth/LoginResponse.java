e14d6d8d2e7100cb28f4befcc1bf95d12ba291cd
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-12 20:56:04 +0700

    CNPM-42 Spring Security Configuration

--- src/main/java/vn/edu/cnpm/projectsupport/auth/LoginResponse.java
+++ src/main/java/vn/edu/cnpm/projectsupport/auth/LoginResponse.java
@@ -1,10 +1,12 @@
 package vn.edu.cnpm.projectsupport.auth;
 
 public class LoginResponse {
-
+    private String accessToken;
+    private String tokenType = "Bearer";
     private String username;
     private String email;
     private String fullName;
+    private String role;
 
     public LoginResponse() {
     }
@@ -13,6 +15,38 @@ public class LoginResponse {
         this.username = username;
         this.email = email;
         this.fullName = fullName;
+    }
+
+    public LoginResponse(String username, String email, String fullName, String role) {
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public LoginResponse(String accessToken, String username, String email, String fullName, String role) {
+        this.accessToken = accessToken;
+        this.tokenType = "Bearer";
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
     }
 
     public String getUsername() {
@@ -37,5 +71,13 @@ public class LoginResponse {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
