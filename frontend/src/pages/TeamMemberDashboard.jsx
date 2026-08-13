38e9d65d1be573c24c41d97ddb838cd2a6cad324
NguyenHuong15 <huongntm2632@ut.edu.vn>
2026-08-13 23:32:42 +0700

    Create TeamMemberDashboard.jsx

---
+++ frontend/src/pages/TeamMemberDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TeamMemberDashboard() {
+    return (
+        <div style={{ padding: "40px", fontFamily: "Arial" }}>
+            <h1>Team Member Dashboard</h1>
+            <p>Chào mừng Team Member!</p>
+        </div>
+    );
+}
+
+export default TeamMemberDashboard;
