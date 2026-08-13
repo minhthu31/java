6044c9440cc000cc4ccc150b49feaa37f0710b9d
NguyenHuong15 <huongntm2632@ut.edu.vn>
2026-08-13 23:31:35 +0700

    Create TeamLeaderDashboard.jsx

---
+++ frontend/src/pages/TeamLeaderDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TeamLeaderDashboard() {
+    return (
+        <div style={{ padding: "40px", fontFamily: "Arial" }}>
+            <h1>Team Leader Dashboard</h1>
+            <p>Chào mừng Team Leader!</p>
+        </div>
+    );
+}
+
+export default TeamLeaderDashboard;
