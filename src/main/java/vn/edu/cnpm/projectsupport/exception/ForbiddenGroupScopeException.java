b828a463caf7bf4b3220b7a76973f8d2899c2918
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-17 15:52:56 +0700

    Create ForbiddenGroupScopeException.java

---
+++ src/main/java/vn/edu/cnpm/projectsupport/exception/ForbiddenGroupScopeException.java
@@ -0,0 +1,7 @@
+package vn.edu.cnpm.projectsupport.exception;
+
+public class ForbiddenGroupScopeException extends RuntimeException {
+    public ForbiddenGroupScopeException(String message) {
+        super(message);
+    }
+}
