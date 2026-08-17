221a60eed822aac655ff269a568be4c9eceb68db
vyhpy2223-hub <vyhpy2223@ut.edu.vn>
2026-08-17 20:32:48 +0700

    CNPM-56 Build Requirement Service

---
+++ src/main/java/vn/edu/cnpm/projectsupport/project/Project.java
@@ -0,0 +1,42 @@
+package vn.edu.cnpm.projectsupport.project;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String groupId;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
