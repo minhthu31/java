252562b168a03f8582d2aee8a43c905ba5378124
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-19 18:30:55 +0700

    Delete src/main/java/vn/edu/cnpm/projectsupport/exception/ForbiddenGroupScopeException.java

--- src/main/java/vn/edu/cnpm/projectsupport/exception/ForbiddenGroupScopeException.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.cnpm.projectsupport.exception;
-
-public class ForbiddenGroupScopeException extends RuntimeException {
-    public ForbiddenGroupScopeException(String message) {
-        super(message);
-    }
-}
