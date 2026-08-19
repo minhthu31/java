90d1788fef1c529d26c21d93d9240532dd6c9e25
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-19 19:24:01 +0700

    Update RequirementValidationAndExceptionTests.java

--- src/test/java/vn/edu/cnpm/projectsupport/RequirementValidationAndExceptionTests.java
+++ src/test/java/vn/edu/cnpm/projectsupport/RequirementValidationAndExceptionTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,7 +29,7 @@ public class RequirementValidationAndExceptionTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private RequirementService requirementService;
 
     @Test
