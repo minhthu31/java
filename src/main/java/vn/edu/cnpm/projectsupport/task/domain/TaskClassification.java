e7e31106c2d1a0b28dc3ab39fd923e0bac1f6a53
ASCII <luuhuunhut66@gmail.com>
2026-08-20 19:07:02 +0700

    CNPM-58: enable Flyway for repository tests

---
+++ src/main/java/vn/edu/cnpm/projectsupport/task/domain/TaskClassification.java
@@ -0,0 +1,9 @@
+package vn.edu.cnpm.projectsupport.task.domain;
+
+public enum TaskClassification {
+    NEW_FEATURE,
+    FEATURE_RELATED,
+    AUTO_TEST,
+    AUTO_LOG,
+    OTHER
+}
---
+++ src/main/java/vn/edu/cnpm/projectsupport/task/domain/TaskIssueType.java
@@ -0,0 +1,9 @@
+package vn.edu.cnpm.projectsupport.task.domain;
+
+public enum TaskIssueType {
+    EPIC,
+    STORY,
+    TASK,
+    BUG,
+    SUBTASK
+}
