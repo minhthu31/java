5a0ea26b4695831abed02e4d5d2acdb249ef5472
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-22 15:05:45 +0700

    Update TaskControllerTest.java

--- src/test/java/vn/edu/cnpm/projectsupport/task/TaskControllerTest.java
+++ src/test/java/vn/edu/cnpm/projectsupport/task/TaskControllerTest.java
@@ -16,8 +16,10 @@ import vn.edu.cnpm.projectsupport.task.dto.UpdateTaskStatusRequest;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TaskController.class)
 class TaskControllerTest {
