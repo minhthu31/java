755ddf1f65ae0e7f0a771174ef171d57873231e5
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-22 15:09:59 +0700

    Update TaskControllerTest.java

--- src/test/java/vn/edu/cnpm/projectsupport/task/TaskControllerTest.java
+++ src/test/java/vn/edu/cnpm/projectsupport/task/TaskControllerTest.java
@@ -1,24 +1,14 @@
 package vn.edu.cnpm.projectsupport.task;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import vn.edu.cnpm.projectsupport.task.dto.CreateTaskRequest;
-import vn.edu.cnpm.projectsupport.task.dto.UpdateTaskStatusRequest;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TaskController.class)
@@ -26,9 +16,6 @@ class TaskControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockBean
     private TaskService taskService;
@@ -40,32 +27,6 @@ class TaskControllerTest {
     @WithMockUser(roles = "TEAM_MEMBER")
     void getTasks_Success_WhenTeamMember() throws Exception {
         mockMvc.perform(get(BASE_URL))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("POST - Forbidden (403) when user is TEAM_MEMBER")
-    @WithMockUser(roles = "TEAM_MEMBER")
-    void createTask_Forbidden_WhenTeamMember() throws Exception {
-        CreateTaskRequest request = new CreateTaskRequest();
-
-        mockMvc.perform(post(BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @DisplayName("PATCH Status - Successful when user is TEAM_MEMBER")
-    @WithMockUser(roles = "TEAM_MEMBER")
-    void updateTaskStatus_Success_WhenTeamMember() throws Exception {
-        UpdateTaskStatusRequest request = new UpdateTaskStatusRequest();
-
-        when(taskService.updateStatus(eq(1L), eq(10L), any())).thenReturn(null);
-
-        mockMvc.perform(patch(BASE_URL + "/10/status")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
     }
 }
