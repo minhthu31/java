41a198d8eef315d1c835b2efa59216f952b7f4c1
hanntn3212-oss <hanntn3212@ut.edu.vn>
2026-08-22 15:06:14 +0700

    Update RequirementControllerTests.java

--- src/test/java/vn/edu/cnpm/projectsupport/requirement/RequirementControllerTests.java
+++ src/test/java/vn/edu/cnpm/projectsupport/requirement/RequirementControllerTests.java
@@ -3,8 +3,7 @@ package vn.edu.cnpm.projectsupport.requirement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,8 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(RequirementController.class)
 class RequirementControllerTests {
 
     @Autowired
