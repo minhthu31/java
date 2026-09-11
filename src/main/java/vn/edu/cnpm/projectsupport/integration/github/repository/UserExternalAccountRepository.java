f1fe6c11143322df475d93aa5a1f398c81dcb5e1
ASCII <luuhuunhut66@gmail.com>
2026-09-11 07:52:06 +0700

    CNPM-103 fix
    ean
    mvn clean test
    
    Please enter the commit message for your changes. Lines starting

--- src/main/java/vn/edu/cnpm/projectsupport/integration/github/repository/UserExternalAccountRepository.java
+++ src/main/java/vn/edu/cnpm/projectsupport/integration/github/repository/UserExternalAccountRepository.java
@@ -1,6 +1,9 @@
 package vn.edu.cnpm.projectsupport.integration.github.repository;
 
+<<<<<<< HEAD
 import java.util.List;
+=======
+>>>>>>> 8571ab8 (CNPM-103 fix)
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.edu.cnpm.projectsupport.integration.github.domain.UserExternalAccount;
@@ -9,7 +12,13 @@ import vn.edu.cnpm.projectsupport.integration.jira.domain.IntegrationProvider;
 public interface UserExternalAccountRepository extends JpaRepository<UserExternalAccount, Long> {
     Optional<UserExternalAccount> findByProviderAndExternalUserId(IntegrationProvider provider, String externalUserId);
 
+<<<<<<< HEAD
     Optional<UserExternalAccount> findByUserIdAndProvider(Long userId, IntegrationProvider provider);
             
     List<UserExternalAccount> findAllByUserIdInAndProvider(List<Long> userIds, IntegrationProvider provider);
+=======
+    Optional<UserExternalAccount> findByUserIdAndProvider(
+            Long userId,
+            IntegrationProvider provider);
+>>>>>>> 8571ab8 (CNPM-103 fix)
 }
